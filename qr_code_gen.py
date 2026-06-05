472f95dc1bca0fbfb5e99105ae6b6ebdf49c600e
sakshichavan28 <sakshichavan451@gmail.com>
2026-06-05 18:46:54 +0530

    first commitit

---
+++ qr_code_gen.py
@@ -0,0 +1,18 @@
+import qrcode
+
+website_link = input("enter the website link")
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr.add_data(website_link)
+qr.make()
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+import qrcode
+
+
+
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr.add_data(website_link)
+qr.make()
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+img.save('youtube_qr.png')
+
